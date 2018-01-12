7f2f2dfc7107263b851a4dd82bb1c8b796ebc775
chris4824 <chris482422@gmail.com>
2018-01-12 11:01:57 -0500

    Revert "sanders: cleanup libmot_gpu_mapper shim"
    This reverts commit 087830fded7f4ad7e7710f2e27a774242afe43e5.

--- device.mk
+++ device.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbson \
     Snap \
+    libshim_camera \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
