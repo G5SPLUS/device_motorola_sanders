ca44e5b8bd60e362201ef37c1c6325536bdc4356
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-16 15:32:08 -0200

    sanders: [1/2] Move DTV blobs to vendor

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -16,13 +16,15 @@ if [ "$sku" = "XT1806" ]; then
     rm -r /system/app/NfcNci
 fi
 
-#if ! [ "$sku" = "XT1683" ]; then
-#    # Others variants doesn't have DTV support
-#    rm /system/etc/permissions/com.motorola.hardware.dtv.xml
-#    rm /system/etc/permissions/mot_dtv_permissions.xml
-#    rm /system/lib/libdtvhal.so
-#    rm /system/lib/libdtvtuner.so
-#    rm -r /system/priv-app/DTVPlayer
-#    rm -r /system/priv-app/DTVService
-#fi
+if ! [ "$sku" = "XT1683" ]; then
+    # Others variants doesn't have DTV support
+    rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
+    rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
+    rm /system/vendor/lib/libdtvtuner.so
+    rm /system/vendor/lib64/libdtvtuner.so
+    rm /system/vendor/lib/libdtvhal.so
+    rm /system/vendor/lib64/libdtvhal.so
+    rm -r /system/vendor/app/DTVPlayer
+    rm -r /system/vendor/app/DTVService
+fi
 
